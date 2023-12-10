57af09aea41e51a2204a4d2d991a33486b91285e
jharahul968 <jharahul968@gmail.com>
2023-12-11 00:32:09 +0545

    need to fix the type errors of detection

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, WebSocket, UploadFile, HTTPException
 from segmentation import get_yolov5, get_image_from_bytes
 from starlette.responses import Response
 import io
 from PIL import Image
 import json
 from fastapi.middleware.cors import CORSMiddleware
+import cv2
+import os
+import shutil
+import base64
 
 model = get_yolov5()
 
@@ -17,6 +21,7 @@ app = FastAPI(
 origins = [
     "http://localhost",
     "http://localhost:8000",
+    "http://localhost:3000",
     "*"
 ]
 app.add_middleware(
@@ -26,12 +31,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.get('/notify/v1/health')
-def get_health():
-    return dict(msg='OK')
-
 
 @app.post("/object-to-json")
 async def detect_polyps_return_json_result(file: bytes = File(...)):
@@ -53,3 +52,87 @@ async def detect_polyps_return_base64_img(file: bytes = File(...)):
         img_base64.save(bytes_io, format="jpeg")
     return Response(content=bytes_io.getvalue(),
                     media_type="image/jpeg")
+
+
+
+UPLOAD_FOLDER= 'uploads'
+ALLOWED_EXTENSIONS={'mp4', 'avi', 'mkv', 'jpg', 'jpeg', 'png'}
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+    
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.post('/send-videos')
+async def extract_frames(file: UploadFile = File(...)):
+    
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="No selected file")
+
+    if not allowed_file(file.filename):
+        raise HTTPException(status_code=400, detail="Invalid file format")
+
+    filename=file.filename
+    filepath=os.path.join(UPLOAD_FOLDER, filename)
+
+    with open(filepath, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {'ack': True, 'filepath': filepath}
+
+
+
+async def detect_polyps_return_img(websocket: WebSocket, frame):
+    
+    input_image = io.BytesIO(frame.encode('utf-8'))
+    pil_image = Image.open(input_image)
+    results = model(input_image)
+    results.render()  # updates results.imgs with boxes and labels
+    for img in results.ims:
+        bytes_io = io.BytesIO()
+        img_base64 = Image.fromarray(img)
+        img_base64.save(bytes_io, format="jpeg")
+    img_base64=bytes_io.getvalue()
+    await websocket.send_text(img_base64)
+
+
+async def extract_frames_from_video(websocket, video_path):
+
+    cap = cv2.VideoCapture(video_path)
+
+    while True:
+
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        # Convert frame to base64 for easy transmission
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_base64 = base64.b64encode(buffer).decode('utf-8')
+        await detect_polyps_return_img(websocket, frame_base64)
+        
+        # await websocket.send_text(frame_base64)
+
+
+    cap.release()
+    
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    
+    await websocket.accept()
+    
+    
+    while True:
+        video_path=await websocket.receive_text()
+        await extract_frames_from_video(websocket, video_path)
+
+
+
+
+
+
+
+
+
