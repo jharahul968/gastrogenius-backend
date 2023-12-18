f8549586e555bcee0f6e0cba60c3eb12dd26f343
jharahul968 <jharahul968@gmail.com>
2023-12-18 18:04:58 +0545

    try to get yolo v5 model working

--- main2.py
+++ main2.py
@@ -7,15 +7,17 @@ from flask import Flask, jsonify
 import cv2
 from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO, emit
-from segmentation import get_yolov5, get_yolov8
+from segmentation import get_yolov8, get_yolov5
 
-model = get_yolov8()
+model=get_yolov5()
+# model = get_yolov8()
 
 app = Flask(__name__)
 cors = CORS(app)
 
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 @socketio.on('connect_with_frontend')
 def handle_connect():
@@ -24,6 +26,7 @@ def handle_connect():
     established between frontend and this server.
     """
     print('Frontend connected to AI Server.')
+
 
 @socketio.on("frame_incoming")
 @cross_origin()
@@ -35,41 +38,43 @@ def extract_frames_from_video(frame):
     """
     # pylint: disable=no-member
     frame_bytes = base64.b64decode(frame)
-    frame_np = np.frombuffer(frame_bytes,dtype=np.uint8)
-    decoded_frame = cv2.imdecode(frame_np,flags=cv2.IMREAD_COLOR)
+    frame_np = np.frombuffer(frame_bytes, dtype=np.uint8)
+    decoded_frame = cv2.imdecode(frame_np, flags=cv2.IMREAD_COLOR)
     rgb_frame = cv2.cvtColor(decoded_frame, cv2.COLOR_BGR2RGB)
     # pylint: enable=no-member
     pil_image = Image.fromarray(rgb_frame)
-    input_image = pil_image
+    # input_image = pil_image
+    input_image = np.array(rgb_frame)
 
     # for yolov5
-    # results = model(input_image)
-    # results.render()  # updates results.imgs with boxes and labels
-    
+    results = model(input_image)
+    results.render()  # updates results.imgs with boxes and labels
+
     # for yolov8
-    results = model(input_image, conf=0.25) 
+    # results = model(input_image, conf=0.25)
 
     # Process images in parallel
-#    for img in results.ims:
-#        img_base64 = Image.fromarray(img)
-#        image_bytes = io.BytesIO()
-#        img_base64.save(image_bytes, format="jpeg")
-#        base64_string = base64.b64encode(image_bytes.getvalue()).decode("utf-8")
-#        emit("Processed_Frame", base64_string)
-
-
-    for r in results:
-        im_array = r.plot()  # plot a BGR numpy array of predictions
-        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-      #  im.show()  # show image
-      #  im.save('results.jpg')  # save image
-        img_base64 = Image.fromarray(im)
+    for img in results.ims:
+        img_base64 = Image.fromarray(img)
         image_bytes = io.BytesIO()
         img_base64.save(image_bytes, format="jpeg")
-        base64_string = base64.b64encode(image_bytes.getvalue()).decode("utf-8")
+        base64_string = base64.b64encode(
+            image_bytes.getvalue()).decode("utf-8")
         emit("Processed_Frame", base64_string)
 
-    return jsonify({"success":True})
+    # for r in results:
+    #     im_array = r.plot()  # plot a BGR numpy array of predictions
+    #     im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+    #   #  im.show()  # show image
+    #   #  im.save('results.jpg')  # save image
+    #     img_base64 = Image.fromarray(im)
+    #     image_bytes = io.BytesIO()
+    #     img_base64.save(image_bytes, format="jpeg")
+    #     base64_string = base64.b64encode(image_bytes.getvalue()).decode("utf-8")
+    #     emit("Processed_Frame", base64_string)
+
+    return jsonify({"success": True})
+
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port = 8000, debug=True)
+    socketio.run(app, host='127.0.0.1', port=8000, debug=True)
--- segmentation.py
+++ segmentation.py
@@ -16,13 +16,23 @@ def get_yolov8():
     return model
 
 
-def get_image_from_bytes(binary_image, max_size=1024):
+# def get_image_from_bytes(binary_image, max_size=1024):
     
-    transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize((max_size, max_size)),
-        ])
+#     transform = transforms.Compose([
+#             transforms.ToTensor(),
+#             transforms.Resize((max_size, max_size)),
+#         ])
 
+#     input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
+#     resized_image = transform(input_image)
+#     return resized_image
+
+def get_image_from_bytes(binary_image, max_size=1024):
     input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
-    resized_image = transform(input_image)
+    width, height = input_image.size
+    resize_factor = min(max_size / width, max_size / height)
+    resized_image = input_image.resize((
+        int(input_image.width * resize_factor),
+        int(input_image.height * resize_factor)
+    ))
     return resized_image
