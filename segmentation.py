2fb5d441fd7fa70f307d32e572cc1cc431940275
Rajesh076055 <imrajeshneupane@gmail.com>
2023-12-26 18:14:55 +0545

    Updated the Backend

--- segmentation.py
+++ segmentation.py
@@ -2,21 +2,26 @@ import torch
 from PIL import Image
 import io
 from torchvision import transforms
+from ultralytics import YOLO
+
 
 def get_yolov5():
     model = torch.hub.load('./yolov5', 'custom',
-                           path='./model/best.pt', source='local')
-    model.conf = 0.1
+                           path='./model/best.pt', source='local',force_reload=True)
+    model.conf = 0.5
+    #model.cuda()
     return model
 
+# def get_yolov8():
+#     model = YOLO('./model/best_v8.pt')
+#     return model
 
-def get_image_from_bytes(binary_image, max_size=640):
-    
-    transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize((max_size, max_size)),
-        ])
-
+def get_image_from_bytes(binary_image, max_size=1024):
     input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
-    resized_image = transform(input_image)
+    width, height = input_image.size
+    resize_factor = min(max_size / width, max_size / height)
+    resized_image = input_image.resize((
+        int(input_image.width * resize_factor),
+        int(input_image.height * resize_factor)
+    ))
     return resized_image
